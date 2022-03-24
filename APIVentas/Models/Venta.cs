6fd119b464778dab9ecf5678eaa5bc86a4e1b375
Legitshadows <la.beltran19@info.uas.edu.mx>
2022-03-23 19:53:38 -0700

    Added some stuff for get set and a json example

--- APIVentas/Models/Venta.cs
+++ APIVentas/Models/Venta.cs
@@ -2,5 +2,14 @@
 {
     public class Venta
     {
+        #region "Datos"
+        public string Folio { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Cantidad { get; set; }
+        public decimal Total { get; set; }
+        #endregion
+
+        #region "Metodos"
+        #endregion
     }
 }
